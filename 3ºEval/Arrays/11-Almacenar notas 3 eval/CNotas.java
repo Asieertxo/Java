e154b022e2eb5edc476174b719916aa938400034
Asieertxo <82152634+Asieertxo@users.noreply.github.com>
2021-05-29 18:35:20 +0200

    Create CNotas.java

---
+++ 3ºEval/Arrays/11-Almacenar notas 3 eval/CNotas.java
@@ -0,0 +1,13 @@
+class CNotas {
+	
+	CNotas (String nombre, String apellido){
+		this.nombre = nombre;
+		this.apellido = apellido;
+		
+		
+	}
+	
+	private String nombre;
+	private String apellido;
+
+}
