e66f0545b3be8da7efee7e7eb78d3ae89cd6cfc9
Asieertxo <82152634+Asieertxo@users.noreply.github.com>
2021-05-29 17:57:02 +0200

    Create Ejercicio4.java

---
+++ 3ºEval/Excepciones/04-Errores/Ejercicio4.java
@@ -0,0 +1,36 @@
+//Mostrar error que da cuando asignamos valor sin crear el array
+public class Ejercicio4 {
+
+	public static void main(String[] args) {
+		int array[];
+		
+		array[1] = 0;
+
+	}
+
+}
+
+
+//Mostrar error que da cuando asignamos valor a una posicion no valida del array
+public class Ejercicio4 {
+
+	public static void main(String[] args) {
+		int array[] = new int[10];
+		
+		array[11] = 0;
+
+	}
+
+}
+
+
+//metodos para manupular String
+public class Ejercicio4 {
+
+	public static void main(String[] args) {
+		String cadena = null;
+		System.out.println(cadena.length());
+		System.out.println(cadena.substring(14));
+	}
+
+}
